4b9871cc3360e1185bb0828eed336e3fe452e32a
Andre S <andre@shomenetwork.ga>
2021-12-03 13:26:44 -0600

    updated

--- biopymlff/calculators/gebf.py
+++ biopymlff/calculators/gebf.py
@@ -324,7 +324,7 @@ class GEBF(FileIOCalculator):
 
             shutil.copy(self.label + "/" + self.label + ".frg", self.directory)
             shutil.copy(self.label + "/" + self.label + ".gjf", self.directory)
-            shutil.rmtree(self.label)
+            # shutil.rmtree(self.label)
             raise NotImplementedError("BREAK")
                         
 
