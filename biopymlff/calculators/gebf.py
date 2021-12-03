8f84e4955c794b18efae97fd269ff5cdac852a86
Andre S <andre@shomenetwork.ga>
2021-12-03 15:10:45 -0600

    update

--- biopymlff/calculators/gebf.py
+++ biopymlff/calculators/gebf.py
@@ -211,7 +211,7 @@ class GEBF(FileIOCalculator):
     
     def subsystems(self):
         mypath=self.directory
-        files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+        files = [f for f in os.listdir(mypath) if os.path.isfile(os.path.join(mypath, f))]
         files = filter(lambda file: ".xyz" in file or ".sxyz" in file, files)
         subsystems = []
         for file in files:
@@ -341,7 +341,7 @@ class GEBF(FileIOCalculator):
         
         labc_filepath = os.path.join(os.getcwd(), self.label, self.label, self.label + ".labc")
         gebf_parent=os.path.join(os.getcwd(), self.label, self.label + "_subsys")
-        gebf_filepaths = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+        gebf_filepaths = [f for f in os.listdir(mypath) if os.path.isfile(os.path.join(gebf_parent, f))]
         gebf_filepaths = filter(lambda file: "gebf" in file or ".agebf" in file, files)
         force_filepath = os.path.join(os.getcwd(), self.label, self.label, self.label + ".force")
         self.read_energy(labc_filepath=labc_filepath, gebf_filepaths=gebf_filepaths)
--- biopymlff/math/bayesian_test.py
+++ biopymlff/math/bayesian_test.py
@@ -1,16 +1,23 @@
+import unittest
+import os
+
 from ase.atoms import Atoms 
+from ase.io.proteindatabank import read_proteindatabank
 
-from ..
+class Bayesian_Test(unittest.TestCase):
+    
+    def setUp(self):
+        self.mol = read_proteindatabank(os.path.join(os.getcwd(), "data", "systems", "4znn.not_wat.pdb"))
+        self.fingerprint = SOAP(l_max=6, n_max=3, atom_sigma=0.4, r_cutoff=3, radial_scaling=1, cutoff_trans_width=1, central_weight=1,
+            n_sparse=1, delta=1, covariance_type="dot_product", zeta=2.5, species=mol.get_chemical_symbols(), sparse_method="cur_points")
+        # Represents Fingerprints
+        # NOTE: Algorithm begins to fail at around 50 atoms
+        # TODO: Not working because matrix is not invertible
+        self.x = self.fingerprint.to_tensor(self.mol)
+        # Represents Potential Energy
+        self.y = [random.randint(1, 30) for val in np.zeros(shape=(len(x), 1))]
 
+    def test_sigma(self):
+        sigma = get_sigma(self.x, self.y)
+        print("SIGMA " + sigma)
 
-# NOTE: Algorithm begins to fail at around 50 atoms
-mol: Atoms = read_proteindatabank(os.getcwd() + "/data/systems/4znn.not_wat.pdb")
-# Potential Kernel for each atom
-# TODO: Not working because matrix is not invertible
-fingerprint = SOAP(l_max=6, n_max=3, atom_sigma=0.4, r_cutoff=3, radial_scaling=1, cutoff_trans_width=1, central_weight=1,
-    n_sparse=1, delta=1, covariance_type="dot_product", zeta=2.5, species=mol.get_chemical_symbols(), sparse_method="cur_points")
-x = fingerprint.to_tensor(mol)
-y = [random.randint(1, 30) for val in np.zeros(shape=(len(x), 1))]
-
-sigma = get_sigma(x, y)
-print("SIGMA " + str(sigma))
