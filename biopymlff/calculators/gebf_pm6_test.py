946a4451ca0c09b744a461de8300e7106d6c8de7
Andre S <andre@shomenetwork.ga>
2021-12-05 20:29:00 -0600

    update

--- biopymlff/calculators/gebf_pm6_test.py
+++ biopymlff/calculators/gebf_pm6_test.py
@@ -3,7 +3,12 @@ import os
 import unittest
 
 from ..test.factory import suite
+from ..test.protein import Protein_Test
 from ..calculators.gebf_dft import GEBF_DFT
 from ..calculators.gebf_pm6 import GEBF_PM6
 
-suite("GEBF PM6", "GEBF DFT", GEBF_PM6(), GEBF_DFT())
+class GEBF_PM6_Test(Protein_Test):
+        
+    def setUp(self):
+        self.init_variables(os.path.join(proteins_path, "4znn.pdb"), "GEBF PM6", "GEBF DFT", GEBF_PM6(), GEBF_PM6())
+        super().setUp()
