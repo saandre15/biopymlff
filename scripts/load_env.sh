5181c8f8074b5a566365aed979cb55adc223caf9
Andre S <andre@shomenetwork.ga>
2021-12-08 15:09:37 -0600

    update

--- scripts/load_env.sh
+++ scripts/load_env.sh
@@ -1,14 +1,17 @@
+idev
 CPU_COUNT=$(grep -c ^processor /proc/cpuinfo)
 export OMP_NUM_THREADS=$CPU_COUNT 
 
-ENV_TOML="env.toml"
-# Load our programs
-load_gaussian="stoml " + $ENV_TOML + " gaussian.load_gaussian"
-eval $load_gaussian
-load_lsqc="stoml " + $ENV_TOML + " lsqc.load_lsqc"
-eval $load_lsqc
-load_amber="stoml " + $ENV_TOML + " amber.load_amber"
-eval $load_amber 
-load_mopac="stoml " + $ENV_TOML + " mopac.load_mopac"
+# ENV_TOML="env.toml"
+# # Load our programs
+# load_gaussian="stoml " + $ENV_TOML + " gaussian.load_gaussian"
+# eval $load_gaussian
+# load_lsqc="stoml " + $ENV_TOML + " lsqc.load_lsqc"
+# eval $load_lsqc
+# load_amber="stoml " + $ENV_TOML + " amber.load_amber"
+# eval $load_amber 
+# load_mopac="stoml " + $ENV_TOML + " mopac.load_mopac"
+
+load_gaussian
 
 pipenv shell
